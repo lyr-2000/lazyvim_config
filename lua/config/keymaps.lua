9ff9b706a888d60859a764ed5df1eddfaf9b9de2
lyr2000 <1957476540@qq.com>
2023-12-12 23:12:37 +0800

    完善vim配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,27 @@
 -- -- DO NOT USE THIS IN YOU OWN CONFIG!!
 -- -- use `vim.keymap.set` instead
 -- local map = Util.safe_keymap_set
+local map = vim.keymap.set
 
 vim.keymap.set("n", "<S-h>", "^")
 vim.keymap.set("n", "<S-l>", "$")
+--  buffer切换
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-S-j>", "<cmd>resize -2<cr>")
+vim.keymap.set("n", "<A-S-k>", "<cmd>resize +2<cr>")
+vim.keymap.set("n", "<A-S-h>", "<cmd>vertical resize -2<cr>")
+vim.keymap.set("n", "<A-S-l>", "<cmd>vertical resize +2<cr>")
+vim.keymap.set("n", "<A-i>", "<cmd>Telescope oldfiles<cr>")
+vim.keymap.set("n", "<A-i>", "<cmd>Telescope oldfiles<cr>")
+vim.keymap.set("n", "<A-;>", "<C-w>|")
+vim.keymap.set("n", "<A-q>", "<cmd>bd<cr>")
+
+-- buffers
+map("n", "<A-n>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "<A-m>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+local builtin = require('telescope.builtin')
+-- grep search https://github.com/BurntSushi/ripgrep#installation
+map("n", "gf", builtin.live_grep)
