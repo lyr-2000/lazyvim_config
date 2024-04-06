c272ecfb1083fef19faad4f5f3fa0b9c6e6df67b
lyr2000 <1957476540@qq.com>
2024-04-06 23:24:23 +0800

    use default config

--- lua/plugins/code/snippet.lua
+++ lua/plugins/code/snippet.lua
@@ -134,70 +134,70 @@ return {
     keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
     dependencies = { "hrsh7th/nvim-cmp" },
     enabled = enableCompletePlugin,
-    opts = function()
-      local cmp = require("cmp")
-      return {
-        {
-          type = "/",
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = {
-            { name = "buffer" },
-          },
-        },
-        {
-          type = ":",
-          mapping = cmp.mapping.preset.cmdline({
-
-            ["<Down>"] = {
-              c = function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                else
-                  fallback()
-                end
-              end,
-            },
-            ["<Up>"] = {
-              c = function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                else
-                  fallback()
-                end
-              end,
-            },
-            ["<Tab>"] = {
-              c = function()
-                -- auto select the first items
-                cmp.select_next_item()
-                cmp.select_prev_item()
-              end,
-            },
-            ['<CR>'] = {
-              c = cmp.mapping.confirm({
-                select = true,
-              }),
-            },
-          }),
-          sources = cmp.config.sources({
-            { name = "path" },
-          }, {
-            {
-              name = "cmdline",
-              option = {
-                ignore_cmds = { "Man", "!" },
-              },
-            },
-          }),
-        },
-      }
-    end,
-    config = function(_, opts)
-      local cmp = require("cmp")
-      vim.tbl_map(function(val)
-        cmp.setup.cmdline(val.type, val)
-      end, opts)
-    end,
+    -- opts = function()
+    --   local cmp = require("cmp")
+    --   return {
+    --     {
+    --       type = "/",
+    --       mapping = cmp.mapping.preset.cmdline(),
+    --       sources = {
+    --         { name = "buffer" },
+    --       },
+    --     },
+    --     {
+    --       type = ":",
+    --       mapping = cmp.mapping.preset.cmdline({
+    --
+    --         ["<Down>"] = {
+    --           c = function(fallback)
+    --             if cmp.visible() then
+    --               cmp.select_next_item()
+    --             else
+    --               fallback()
+    --             end
+    --           end,
+    --         },
+    --         ["<Up>"] = {
+    --           c = function(fallback)
+    --             if cmp.visible() then
+    --               cmp.select_prev_item()
+    --             else
+    --               fallback()
+    --             end
+    --           end,
+    --         },
+    --         ["<Tab>"] = {
+    --           c = function()
+    --             -- auto select the first items
+    --             cmp.select_next_item()
+    --             cmp.select_prev_item()
+    --           end,
+    --         },
+    --         ['<CR>'] = {
+    --           c = cmp.mapping.confirm({
+    --             select = true,
+    --           }),
+    --         },
+    --       }),
+    --       sources = cmp.config.sources({
+    --         { name = "path" },
+    --       }, {
+    --         {
+    --           name = "cmdline",
+    --           option = {
+    --             ignore_cmds = { "Man", "!" },
+    --           },
+    --         },
+    --       }),
+    --     },
+    --   }
+    -- end,
+    -- config = function(_, opts)
+    --   local cmp = require("cmp")
+    --   vim.tbl_map(function(val)
+    --     cmp.setup.cmdline(val.type, val)
+    --   end, opts)
+    -- end,
   },
 
 }
